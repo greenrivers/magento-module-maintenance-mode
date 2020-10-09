cd2d41c79ecbe5a557e6107fbbabde5e17964efb
MajorKuprich <majorkuprich@gmail.com>
2020-10-09 22:19:22 +0200

    Module init

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author GreenRivers Team
+ * @copyright Copyright (c) 2020 GreenRivers
+ * @package GreenRivers_MaintenanceMode
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'GreenRivers_MaintenanceMode', __DIR__);
